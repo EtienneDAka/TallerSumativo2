db634f997e0ac2f31a13912784836ebe4a4412ef
Etienne Defranc <etiennedefranc@gmail.com>
2025-10-29 13:53:41 -0500

    Se agregaron constructores a clases heredadas
    (Por alguna razón mis commits aparecían con el nombre de Anie2490, pero ya corregimos ese problema)

--- taller/src/main/java/ec/edu/espol/Evaluacion.java
+++ taller/src/main/java/ec/edu/espol/Evaluacion.java
@@ -1,9 +1,12 @@
 package ec.edu.espol;
 
+import java.time.LocalDateTime;
+
 public class Evaluacion extends ActividadSumativa{
     private int limiteDetiempo;
 
-    public Evaluacion(int limiteDetiempo) {
+    public Evaluacion (String titulo, LocalDateTime fechaDeEntrega, float puntajeMaximo, String contenido, String id, int limiteDetiempo) {
+        super(titulo, fechaDeEntrega, puntajeMaximo, contenido, id);
         this.limiteDetiempo = limiteDetiempo;
     }
 }
--- taller/src/main/java/ec/edu/espol/Tarea.java
+++ taller/src/main/java/ec/edu/espol/Tarea.java
@@ -5,5 +5,9 @@ import java.time.LocalDateTime;
 public class Tarea extends ActividadSumativa{
     private LocalDateTime fechaDePublicacion;
 
+    public Tarea(String titulo, LocalDateTime fechaDeEntrega, float puntajeMaximo, String contenido, String id, LocalDateTime fechaDePublicacion) {
+        super(titulo, fechaDeEntrega, puntajeMaximo, contenido, id);
+        this.fechaDePublicacion = fechaDePublicacion;
+    }
 
 }
