f25fb967e640b5345db6af6b4333161e3e7c89e7
JosuePach3co <150095207+JosuePach3co@users.noreply.github.com>
2025-10-29 14:31:21 -0500

    Cambiando clase ActividadSumativa a abstracta
    ..

--- taller/src/main/java/ec/edu/espol/ActividadSumativa.java
+++ taller/src/main/java/ec/edu/espol/ActividadSumativa.java
@@ -2,7 +2,7 @@ package ec.edu.espol;
 
 import java.time.LocalDateTime;
 
-public class ActividadSumativa {
+public abstract class ActividadSumativa {
     protected String titulo;
     protected LocalDateTime fechaDeEntrega;
     protected float puntajeMaximo;
