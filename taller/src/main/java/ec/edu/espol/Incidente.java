be0edc467f6da9d3b45f645f45c03aa8ccbe1d8c
Anahi Q <anahi.quezada2005@gmail.com>
2025-10-29 13:43:50 -0500

    correccion a LocalDateTime en Incidente.java

--- taller/src/main/java/ec/edu/espol/Incidente.java
+++ taller/src/main/java/ec/edu/espol/Incidente.java
@@ -1,15 +1,15 @@
 package ec.edu.espol;
 
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Incidente {
     protected int id;
-    protected DateTimeFormatter fechaHora;
+    protected LocalDateTime fechaHora;
     protected String estado;
     protected String titulo;
     protected String descripcion;
     protected ArrayList<String> log_actualizaciones;
-    protected DateTimeFormatter fecha_cerrado;
+    protected LocalDateTime fecha_cerrado;
     protected String tipo;
 }
